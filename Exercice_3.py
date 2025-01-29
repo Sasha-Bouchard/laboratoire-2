ee156a7b974f29ec0588889511dc8059d0dede03
Sasha <201165090@cegepsherbrooke.qc.ca>
2025-01-29 13:22:47 -0500

    Create Exercice_3.py

---
+++ Exercice_3.py
@@ -0,0 +1,11 @@
+"""Correction de la fonction suivante selon les normes PEP8 
+def fonction (a,b):
+ c=a+b
+ return c
+print( fonction(3,4) )"""
+
+
+def fonction(a, b):
+    c = a + b
+    return(c)
+print(fonction(3, 4))
