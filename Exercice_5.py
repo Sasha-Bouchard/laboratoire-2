b7daa861efe59c205e45f66f4faef1b23ae2b936
Sasha <201165090@cegepsherbrooke.qc.ca>
2025-01-29 13:58:48 -0500

    Create Exercice_5.py
    Fichier Python avec la description du dessin au tableau

---
+++ Exercice_5.py
@@ -0,0 +1,6 @@
+"""
+Le programme débute par se demander s'il doit s'arrêter. Si oui, il s'arrête.
+Sinon, il vérifie si la température est plus petite que la cible à 20 degrés Celsius.
+Si oui, il "allume le chauffage" et puis retourne à la première question.
+Sinon, il y retourne directement
+"""
