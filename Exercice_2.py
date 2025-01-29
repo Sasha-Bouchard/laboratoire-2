016636052694c6e2378847a56e3e01316744fd8c
Sasha <201165090@cegepsherbrooke.qc.ca>
2025-01-29 13:11:28 -0500

    Exercice 2

---
+++ Exercice_2.py
@@ -0,0 +1,6 @@
+#Programme qui convertis un nombre en int, puis en float
+print("Entrer un nombre")
+nombre = float(input()) #sert à évité une mauvaise lecture
+entier = int(nombre)
+print("Ce nombre se convertis en entier ainsi:",entier)
+print("Ce nombre se convertis en float ainsi:",nombre)
